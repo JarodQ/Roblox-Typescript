c99ab1ca6827d1831652ae08dbc2e4e9efa75a6d
JarodQ <jarod.quinlisk@gmail.com>
2025-07-08 22:32:10 -0400

    Missed two files on last commit

--- src/shared/WeaponSystem/Ammo/AmmoType.ts
+++
@@ -1,8 +0,0 @@
-import { WeaponStats } from "../Weapons/Weapon";
-import { WeaponEffect } from "../Effects/WeaponEffect";
-
-export interface AmmoType {
-    name: string;
-    modifiers: Partial<WeaponStats>;
-    effect?: WeaponEffect;
-}
